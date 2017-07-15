fd319c4f9f4dfbadfd8683b10c477940752e4d1b
ARNAB <ARNAB@DESKTOP-6CO1CVI>
2017-07-15 16:01:27 +0530

    First Commit

---
+++ ShoesKartBackend/src/main/java/com/ShoesKart/ShoesKartBackend/model/Product.java
@@ -0,0 +1,82 @@
+package com.ShoesKart.ShoesKartBackend.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+
+import org.springframework.web.multipart.MultipartFile;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	int prodid;
+	String name;
+	int quantity;
+	int price;
+	//int suppid;
+	@ManyToOne //(cascade = CascadeType.PERSIST)
+	Category cat;
+	
+	@ManyToOne
+	Supplier supplier;
+	
+	String description;
+	@Transient
+	MultipartFile pimage;
+	
+	public MultipartFile getPimage() {
+		return pimage;
+	}
+	public void setPimage(MultipartFile pimage) {
+		this.pimage = pimage;
+	}
+	
+	public int getProdid() {
+		return prodid;
+	}
+	public void setProdid(int prodid) {
+		this.prodid = prodid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Category getCat() {
+		return cat;
+	}
+	public void setCat(Category cat) {
+		this.cat = cat;
+	}
+	public Supplier getSupplier() {
+		return supplier;
+	}
+	public void setSupplier(Supplier supplier) {
+		this.supplier = supplier;
+	}
+	
+}
